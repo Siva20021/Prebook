df1ff18a27893f094e4f9c8f1f052fc7c438a916
Sudhanshu Srivastava <ss1163@armist.edu.in>
2022-09-03 14:35:09 +0530

    All movies show page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Routes,Route,BrowserRouter } from "react-router-dom"
 import './App.css';
+import MovieListing from "./pages/MovieListing";
 import TicketBooking from "./pages/TicketBooking";
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<TicketBooking/>}/>
+        <Route path="/movies" element={<MovieListing />} />
       </Routes>
     </div>
   </BrowserRouter>
